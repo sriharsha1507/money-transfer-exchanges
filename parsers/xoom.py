26d886a919ac13ee0396343118597bab28fa54e7
Prudhvi Raj Marboyena <xanthusblack@gmail.com>
2018-10-28 02:21:46 -0500

    Fixed a xoom implementation and made it work

--- parsers/xoom.py
+++ parsers/xoom.py
@@ -1,5 +1,7 @@
 # import libraries
-import re
+import calendar
+import json
+import time
 
 from parsers.helper.scraper import Scraper
 
@@ -11,10 +13,12 @@ class Xoom:
 
     @staticmethod
     def get_amount():
-        target_page = 'https://www.xoom.com/india/send-money'
-        scraper = Scraper(target_page)
-        # xvx-text-right xvx-font-copy is the class name which contains our required data for this project
-        info_tag = scraper.get_html_data().find('p', attrs={'class': 'xvx-text-right xvx-font-copy'})
-        data = re.findall("\d+\.\d+", info_tag.text)
-        xoom_result = {"amount": data}
-        return xoom_result
+        # info_tag = scraper.get_html_data().find('p', attrs={'class': 'xvx-text-right xvx-font-copy'})
+        # data = re.findall("\d+\.\d+", info_tag.text)
+        target_page = 'https://www.xoom.com/calculate-fee-table?sourceCountryCode=US&sourceCurrencyCode=USD&destinationCountryCode=IN&destinationCurrencyCode=INR&sendAmount=1.00&receiveAmount=72.00&localCurrency=true&serviceType=&serviceSlug=&receiveAmountEntered=false&oldSourceCurrencyCode=USD&oldDestinationCurrencyCode=INR&remittanceResourceID=207115e0-38f8-408a-a3a5-493476792d70&_=' + str(
+            calendar.timegm(time.gmtime()) * 1000)
+        response = Scraper(target_page)
+        result = response.get_html_data()
+        json_string = result.find('data', attrs={'id': 'jsonData'}).text
+        j_data = json.loads(json_string)
+        return {'amount': j_data['data']['fxRate']}
